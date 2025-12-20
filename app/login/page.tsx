698702f794c4b6dec66d71df3cac13256955038a
rudrash170 <rudra.sh170@gmail.com>
2025-12-21 01:23:40 +0530

    loginpage and pnpm issue

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,8 @@ import { AuthForm } from "@/components/auth-form"
 
 export default function LoginPage() {
   return (
-    <div className="container flex h-screen w-screen flex-col items-center justify-center">
-      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+    <div className="flex min-h-[calc(100vh-8rem)] w-full flex-col items-center justify-center px-4">
+      <div className="flex w-full max-w-[350px] flex-col justify-center space-y-6">
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-2xl font-semibold tracking-tight">
             Sign in to your account
